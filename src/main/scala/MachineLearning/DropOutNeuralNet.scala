1a186a8097430773f932b9f0536022c859f66bb7
raghvendra.singh <raghvendra.singh@insidesales.com>
2016-01-22 08:49:56 -0700

    Refactored code for DropOutNeuralNet.scala to incorporate softmax classifier.

--- src/main/scala/MachineLearning/DropOutNeuralNet.scala
+++ src/main/scala/MachineLearning/DropOutNeuralNet.scala
@@ -7,6 +7,7 @@ package MachineLearning
 import breeze.linalg._
 
 object ExecuteDropOutNeuralNet extends App {
-  NNUtils.initializeNet(Vector(4,3,2))
-  NNUtils.train(DenseMatrix((1.0,1.0),(2.0,2.0),(-1.0,-1.0),(-2.0,-2.0)), DenseMatrix((0.0,1.0),(1.0,0.0)), 1, 0.1, 0.3, true, false, 0.5)
+  NNUtils.initializeNet(Vector(5,4,4,3))
+  /** For Softmax output make sure that training labels are in one hot vector format. */
+  NNUtils.train(DenseMatrix((1.0,1.5),(2.0,2.2),(-1.0,-1.6),(-2.0,-2.1),(-3.0,-3.4)), DenseMatrix((0.0,1.0),(1.0,0.0),(0.0,0.0)), "SE", 1, 0.1, 0.3, true)
 }
