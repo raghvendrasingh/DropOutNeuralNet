94c557cba39c4bab0c4d04b102bac36472e721b4
raghvendra.singh <raghvendra.singh@insidesales.com>
2016-01-28 22:48:41 -0700

    Added code for softmax classifier and refactored code for DropOutNeuralNet.scala

--- src/main/scala/MachineLearning/SoftmaxClassifier.scala
+++ src/main/scala/MachineLearning/SoftmaxClassifier.scala
@@ -1,26 +1,25 @@
 package MachineLearning
 
 /**
-  * Created by raghvendra.singh on 1/9/16.
+  * Created by raghvendra.singh on 1/28/16.
   */
-
 import breeze.linalg._
 
-object ExecuteDropOutNeuralNet extends App {
-  NNUtils.initializeNet(Vector(2,3,3))
+object ExecuteSoftmaxClassifier extends App {
+  NNUtils.initializeNet(Vector(5,4,3))
   /** Training data with one sample per column. No. of rows = No. of features per sample. */
-  val trainingSamples = DenseMatrix((1.0,1.5,2.0,2.2,-1.0),(-1.6,-2.0,-2.1,-3.0,-3.4))
-  /** Training output with one output sample per column. No. of rows = No. of output units in neural net. */
-  val trainingLabels = DenseMatrix((0.2,1.0,1.1,1.5,2.1),(1.0,2.1,1.4,0.4,0.6),(0.7,0.2,1.1,1.5,1.9))
+  val trainingSamples = DenseMatrix((1.0,1.5),(2.0,2.2),(-1.0,-1.6),(-2.0,-2.1),(-3.0,-3.4))
+  /** Training output with one output sample per column. No. of rows = No. of output units in neural net.
+    * For Softmax output make sure that training labels are in one hot vector format.
+    */
+  val trainingLabels = DenseMatrix((0.0,1.0),(1.0,0.0),(0.0,0.0))
   /** Maximum number of iterations while training the model */
   val maxEpochs = 1
   /** Learning rate used in the optimization algorithm. */
   val learningRate = 0.1
   /** It is a regularization parameter to avoid over fitting in neural net. */
-  val weightDecay = 0.2
-  /** This a boolean flag to indicate numerical gradient check. If it is true then the algorithm executes for only
-    * one iteration just to make sure that gradient through back propagation is calculated correctly
-    */
+  val weightDecay = 0.3
+  /** This a boolean flag to indicate numerical gradient check. */
   val checkNumericalGrad = true
   /** This is a boolean flag to indicate use of dropout neural net. */
   val isDropOut = false
@@ -42,13 +41,14 @@ object ExecuteDropOutNeuralNet extends App {
       val dropNodeList = NNUtils.makeDropNodes(numSamples, dropOutProbability, isDropOut)
       val shuffledTuple = NNUtils.shuffleMatrix(trainingSamples, trainingLabels)
       NNUtils.forward(shuffledTuple._1, dropNodeList)
-      /** Output layer uses non linear sigmoid function.*/
-      NNUtils.meanSquaredErrorLossForward()
-      /** We are trying to minimize mean squared error cost function */
-      val outputDelta: DenseMatrix[Double] = NNUtils.meanSquaredErrorLossBackward(shuffledTuple._2)
+      /** Output layer uses softmax function.*/
+      NNUtils.softmaxLossForward()
+      /** We are trying to minimize softmax cost function */
+      val outputDelta = NNUtils.softmaxLossBackward(shuffledTuple._2)
       NNUtils.backward(numSamples, outputDelta, dropNodeList, weightDecay)
       if (checkNumericalGrad) {
-        val numGrad: DenseVector[Double] = NNUtils.computeNumericalGradientMSE(numSamples, shuffledTuple._1, shuffledTuple._2, weightDecay)
+        var numGrad: DenseVector[Double] = null
+        numGrad = NNUtils.computeNumericalGradientSE(numSamples, shuffledTuple._1, shuffledTuple._2, weightDecay)
         NNUtils.checkNumericalGradient(numGrad)
         epoch = 1
       }
